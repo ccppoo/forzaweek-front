2a3eb877a05cbb863a82830a2e49e28aa6966e22
ccppoo <shmishmi79@gmail.com>
2024-05-24 00:11:28 +0900

    add component

---
+++ src/components/YearCard/YearCard.tsx
@@ -0,0 +1,45 @@
+import { useNavigate } from 'react-router-dom';
+
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Unstable_Grid2';
+import { styled } from '@mui/material/styles';
+
+import { FlexBox, FullSizeCenteredFlexBox } from '@/components/styled';
+
+function get_year_color(year: number): string {
+  if (year < 1930) return '#AB933C';
+  if (year < 1940) return '#345D98';
+  if (year < 1950) return '#B38069';
+  if (year < 1960) return '#68BBB8';
+  if (year < 1970) return '#758C33';
+  if (year < 1980) return '#CA7CD8';
+  if (year < 1990) return '#287E9E';
+  if (year < 2000) return '#E33056';
+  if (year < 2010) return '#2BD566';
+  if (year < 2020) return '#85e309';
+  if (year < 2030) return '#093ce3';
+  return '#1ed6d9';
+}
+
+export default function YearCard({ year }: { year: number }) {
+  const YEAR_COLOR = get_year_color(year);
+  return (
+    <FlexBox
+      sx={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        minWidth: 50,
+        minHeight: 25,
+        backgroundColor: YEAR_COLOR,
+        borderRadius: 1,
+      }}
+    >
+      <Typography fontSize={15} fontWeight="fontWeightMedium" color="white">
+        {year}
+      </Typography>
+    </FlexBox>
+  );
+}
---
+++ src/components/YearCard/index.ts
@@ -0,0 +1,3 @@
+import YearCard from './YearCard';
+
+export default YearCard;
