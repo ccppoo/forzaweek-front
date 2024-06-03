74acdf28cb0bba0e29fd6b0e326299d167be1960
ccppoo <shmishmi79@gmail.com>
2024-06-03 15:09:36 +0900

    Update CarDetailInfo.tsx

--- src/components/Car/CarDetailInfo.tsx
+++ src/components/Car/CarDetailInfo.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { Box, Divider } from '@mui/material';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
@@ -10,54 +11,106 @@ import * as image from '@/image';
 import { FlexBox } from '@/components/styled';
 import { Image } from '@/components/styled';
 
+function CarDetailInfoCell({
+  title,
+  value,
+  image,
+}: {
+  title: string;
+  value: string | number;
+  image?: string;
+}) {
+  return (
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: '220px',
+        gridTemplateRows: '35px 5px 50px',
+      }}
+    >
+      <FlexBox sx={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Typography sx={{ fontSize: 18, fontWeight: 200 }}>{title}</Typography>
+      </FlexBox>
+      <FlexBox sx={{ width: '100%', justifyContent: 'center', alignItems: 'start' }}>
+        <Divider variant="middle" sx={{ margin: 0 }} />
+      </FlexBox>
+      <FlexBox sx={{ columnGap: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {image && (
+          <FlexBox
+            sx={{
+              height: 50,
+            }}
+          >
+            <Image
+              src={image}
+              sx={{
+                width: 'auto',
+                height: '100%',
+                // maxHeight: '100%',
+              }}
+            />
+          </FlexBox>
+        )}
+        <FlexBox sx={{ alignItems: 'center' }}>
+          <Typography sx={{ fontSize: 21, fontWeight: 400 }}>{value}</Typography>
+        </FlexBox>
+      </FlexBox>
+    </Box>
+  );
+}
+
 export default function CarDetailInfo() {
   const WIDTH = '100%';
-  const HEIGHT = '100%';
-  const name = 'Arch of Mulegé Circuit';
-  const road_type = 'road';
-  const track_type = 'circuit';
-  const laps = 3;
-  const description = 'design of Hyundai elantra, its my style';
+  const HEIGHT = 100;
+  const manufacturer = 'Hyundai';
+  const country = 'Korea';
+  const division = 'Super Hot Hatch';
+  const prodYear = 2020;
+  const bodyStyle = 'Sedan';
+  // "engine": "ICE",
+  //   "division": "super hot hatch",
+
+  const detailInfo = [
+    {
+      title: 'Manufacturer',
+      value: manufacturer,
+      image: image.manufacturer.hyundai,
+    },
+    {
+      title: 'Country',
+      value: country,
+      image: image.flags.korea,
+    },
+    {
+      title: 'Production Year',
+      value: prodYear,
+      // image: image.manufacturer.hyundai,
+    },
+    {
+      title: 'Division',
+      value: division,
+      // image: image.manufacturer.hyundai,
+    },
+    // {
+    //   title: 'Body Style',
+    //   value: bodyStyle,
+    //   // image: image.manufacturer.hyundai,
+    // },
+  ];
 
   return (
-    <FlexBox sx={{ width: '100%', height: '100%' }}>
-      <Grid container sx={{ paddingTop: 1 }}>
-        {/* 차 사진 */}
-        <Grid xs={4} sx={{}}>
-          <Image src={image.car.hyundaiElantra} sx={{ objectFit: 'contain' }} />
-        </Grid>
-        {/* 차 특징 설명 */}
-        <Grid
-          xs={8}
-          sx={{
-            height: '100%',
-            flexDirection: 'column',
-            paddingLeft: 2,
-          }}
-        >
-          <FlexBox sx={{ flexDirection: 'column' }}>
-            <Typography variant="h6">Description</Typography>
-            <Typography variant="body1">{description}</Typography>
-          </FlexBox>
-          <FlexBox sx={{ flexDirection: 'column' }}>
-            <FlexBox sx={{ alignItems: 'center', columnGap: 1 }}>
-              <Typography variant="h6">Road Type : </Typography>
-              <Typography variant="h6">{road_type}</Typography>
-            </FlexBox>
-            <FlexBox sx={{ alignItems: 'center', columnGap: 1 }}>
-              <Typography variant="h6">Track Type : </Typography>
-              <Typography variant="h6">{track_type}</Typography>
-              <Typography>laps: {laps}</Typography>
-            </FlexBox>
-            <FlexBox sx={{ flexDirection: 'column', columnGap: 1 }}>
-              <FlexBox sx={{ alignItems: 'center', columnGap: 1 }}>
-                <Typography variant="h6">Tags : </Typography>
-                <Typography variant="h6">{track_type}</Typography>
-              </FlexBox>
-            </FlexBox>
-          </FlexBox>
-        </Grid>
-      </Grid>
+    <FlexBox
+      sx={{
+        width: '100%',
+        height: '100%',
+        flexWrap: 'wrap',
+        rowGap: 3,
+        justifyContent: 'space-around',
+      }}
+    >
+      {detailInfo.map((info) => {
+        return <CarDetailInfoCell {...info} />;
+      })}
     </FlexBox>
   );
 }
