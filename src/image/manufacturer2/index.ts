9c561f7cd5cb505fae56b00f1e38980a1caa2416
ccppoo <shmishmi79@gmail.com>
2024-06-04 22:10:35 +0900

    add manufacturer image

---
+++ src/image/manufacturer2/index.ts
@@ -0,0 +1,309 @@
+import International from './International.webp';
+import acura from './acura.png';
+import alfa_romeo from './alfa_romeo.png';
+import alpine from './alpine.png';
+import alumicraft from './alumicraft.png';
+import amc from './amc.png';
+import amg_transport_dynamics from './amg_transport_dynamics.webp';
+import apollo from './apollo.webp';
+import ariel from './ariel.webp';
+import ascari from './ascari.webp';
+import aston_martin from './aston_martin.png';
+import ats from './ats.webp';
+import audi from './audi.webp';
+import austin from './austin.webp';
+import austin_healey from './austin_healey.webp';
+import autounion from './autounion.webp';
+import bac from './bac.webp';
+import baldwin from './baldwin.webp';
+import bentley from './bentley.webp';
+import bertone from './bertone.webp';
+import bmw from './bmw.webp';
+import bowler from './bowler.webp';
+import brabham from './brabham.webp';
+import brm from './brm.webp';
+import bugatti from './bugatti.webp';
+import buick from './buick.webp';
+import cadillac from './cadillac.webp';
+import can_am from './can_am.webp';
+import caparo from './caparo.webp';
+import caterham from './caterham.webp';
+import chaparral from './chaparral.webp';
+import chevrolet from './chevrolet.webp';
+import chrysler from './chrysler.webp';
+import chryslus from './chryslus.webp';
+import citroen from './citroen.webp';
+import czinger from './czinger.webp';
+import datsun from './datsun.webp';
+import delorean from './delorean.webp';
+import detomaso from './detomaso.webp';
+import devon from './devon.webp';
+import dodge from './dodge.webp';
+import donkervoort from './donkervoort.webp';
+import ds_automobiles from './ds_automobiles.webp';
+import eagle from './eagle.webp';
+import eagle_uk from './eagle_uk.webp';
+import ferrari from './ferrari.webp';
+import fiat from './fiat.webp';
+import abarth from './fiat_abarth.png';
+import ford from './ford.webp';
+import formulaE from './formulaE.webp';
+import formula_drift from './formula_drift.webp';
+import funco from './funco.webp';
+import gmc from './gmc.webp';
+import gta from './gta.webp';
+import hennessey from './hennessey.webp';
+import holden from './holden.webp';
+import honda from './honda.webp';
+import hoonigan from './hoonigan.webp';
+import hot_wheels from './hot_wheels.webp';
+import hsv from './hsv.webp';
+import hummer from './hummer.webp';
+import hyundai from './hyundai.webp';
+import infiniti from './infiniti.webp';
+import jaguar from './jaguar.webp';
+import jeep from './jeep.webp';
+import koenigsegg from './koenigsegg.webp';
+import ktm from './ktm.webp';
+import lamborghini from './lamborghini.webp';
+import lancia from './lancia.webp';
+import landRover from './landRover.webp';
+import lexus from './lexus.webp';
+import lincoln from './lincoln.webp';
+import localmotors from './localmotors.webp';
+import lola from './lola.webp';
+import lotus from './lotus.webp';
+import lynk_co from './lynk_&_co.webp';
+import maserati from './maserati.webp';
+import mazda from './mazda.webp';
+import mclaren from './mclaren.webp';
+import mercedesbenz from './mercedesbenz.webp';
+import mercury from './mercury.webp';
+import merkur from './merkur.webp';
+import meyers from './meyers.webp';
+import mg from './mg.webp';
+import mini from './mini.webp';
+import mitsubishi from './mitsubishi.webp';
+import morgan from './morgan.webp';
+import morris from './morris.webp';
+import mosler from './mosler.webp';
+import napier from './napier.webp';
+import nio from './nio.webp';
+import nissan from './nissan.webp';
+import noble from './noble.webp';
+import oldsmobile from './oldsmobile.webp';
+import opel from './opel.webp';
+import pagani from './pagani.webp';
+import panoz from './panoz.webp';
+import peel from './peel.webp';
+import penhall from './penhall.webp';
+import peugeot from './peugeot.webp';
+import plymouth from './plymouth.webp';
+import polaris from './polaris.webp';
+import pontiac from './pontiac.webp';
+import porsche from './porsche.webp';
+import proto from './proto.webp';
+import quadra from './quadra.webp';
+import quartz from './quartz.webp';
+import radical from './radical.webp';
+import raesr from './raesr.webp';
+import ram from './ram.webp';
+import reliant from './reliant.webp';
+import rimac from './rimac.webp';
+import rj from './rj.webp';
+import robby_gordon from './robby_gordon.webp';
+import rollsroyce from './rollsroyce.webp';
+import rossion from './rossion.webp';
+import rover from './rover.webp';
+import ruf from './ruf.webp';
+import saab from './saab.webp';
+import saleen from './saleen.webp';
+import saturn from './saturn.webp';
+import schuppan from './schuppan.webp';
+import scion from './scion.webp';
+import seat from './seat.webp';
+import shelby from './shelby.webp';
+import sierra_cars from './sierra_cars.webp';
+import smart from './smart.webp';
+import spada from './spada.webp';
+import spyker from './spyker.webp';
+import ssc from './ssc.webp';
+import subaru from './subaru.webp';
+import sunbeam from './sunbeam.webp';
+import suzuki from './suzuki.webp';
+import talbot from './talbot.webp';
+import tamo from './tamo.webp';
+import terradyne from './terradyne.webp';
+import tesla from './tesla.webp';
+import topgear from './topgear.webp';
+import toyota from './toyota.webp';
+import triumph from './triumph.webp';
+import tvr from './tvr.webp';
+import ultima from './ultima.webp';
+import vauxhall from './vauxhall.webp';
+import volkswagen from './volkswagen.webp';
+import volvo from './volvo.webp';
+import vuhl from './vuhl.webp';
+import watson from './watson.webp';
+import wiesmann from './wiesmann.webp';
+import wmotors from './wmotors.webp';
+import wuling from './wuling.webp';
+import xpeng from './xpeng.webp';
+import zenvo from './zenvo.webp';
+
+const manufacturer2 = {
+  lancia: lancia,
+  lamborghini: lamborghini,
+  ktm: ktm,
+  koenigsegg: koenigsegg,
+  jeep: jeep,
+  jaguar: jaguar,
+  International: International,
+  infiniti: infiniti,
+  hyundai: hyundai,
+  hummer: hummer,
+  hsv: hsv,
+  hot_wheels: hot_wheels,
+  hoonigan: hoonigan,
+  honda: honda,
+  holden: holden,
+  hennessey: hennessey,
+  gmc: gmc,
+  funco: funco,
+  ford: ford,
+  formulaE: formulaE,
+  formula_drift: formula_drift,
+  fiat: fiat,
+  ferrari: ferrari,
+  eagle_uk: eagle_uk,
+  eagle: eagle,
+  ds_automobiles: ds_automobiles,
+  radical: radical,
+  quartz: quartz,
+  quadra: quadra,
+  proto: proto,
+  porsche: porsche,
+  pontiac: pontiac,
+  polaris: polaris,
+  plymouth: plymouth,
+  peugeot: peugeot,
+  penhall: penhall,
+  peel: peel,
+  panoz: panoz,
+  pagani: pagani,
+  opel: opel,
+  oldsmobile: oldsmobile,
+  noble: noble,
+  nissan: nissan,
+  nio: nio,
+  napier: napier,
+  mosler: mosler,
+  morris: morris,
+  morgan: morgan,
+  mitsubishi: mitsubishi,
+  mini: mini,
+  mg: mg,
+  meyers: meyers,
+  merkur: merkur,
+  mercury: mercury,
+  mercedesbenz: mercedesbenz,
+  mclaren: mclaren,
+  mazda: mazda,
+  maserati: maserati,
+  lynk_co: lynk_co,
+  lotus: lotus,
+  lola: lola,
+  localmotors: localmotors,
+  lincoln: lincoln,
+  lexus: lexus,
+  landRover: landRover,
+  donkervoort: donkervoort,
+  dodge: dodge,
+  devon: devon,
+  delorean: delorean,
+  detomaso: detomaso,
+  datsun: datsun,
+  czinger: czinger,
+  citroen: citroen,
+  chryslus: chryslus,
+  chrysler: chrysler,
+  chevrolet: chevrolet,
+  chaparral: chaparral,
+  caterham: caterham,
+  caparo: caparo,
+  can_am: can_am,
+  cadillac: cadillac,
+  buick: buick,
+  bugatti: bugatti,
+  brm: brm,
+  bac: bac,
+  brabham: brabham,
+  bowler: bowler,
+  bmw: bmw,
+  bertone: bertone,
+  bentley: bentley,
+  baldwin: baldwin,
+  autounion: autounion,
+  austin_healey: austin_healey,
+  austin: austin,
+  audi: audi,
+  ats: ats,
+  ascari: ascari,
+  ariel: ariel,
+  apollo: apollo,
+  amg_transport_dynamics: amg_transport_dynamics,
+  amc: amc,
+  alumicraft: alumicraft,
+  alpine: alpine,
+  alfa_romeo: alfa_romeo,
+  acura: acura,
+  abarth: abarth,
+  aston_martin: aston_martin,
+  zenvo: zenvo,
+  xpeng: xpeng,
+  wuling: wuling,
+  wiesmann: wiesmann,
+  watson: watson,
+  wmotors: wmotors,
+  vuhl: vuhl,
+  volvo: volvo,
+  volkswagen: volkswagen,
+  vauxhall: vauxhall,
+  ultima: ultima,
+  tvr: tvr,
+  triumph: triumph,
+  toyota: toyota,
+  topgear: topgear,
+  tesla: tesla,
+  terradyne: terradyne,
+  tamo: tamo,
+  talbot: talbot,
+  suzuki: suzuki,
+  sunbeam: sunbeam,
+  subaru: subaru,
+  ssc: ssc,
+  spyker: spyker,
+  gta: gta,
+  spada: spada,
+  smart: smart,
+  sierra_cars: sierra_cars,
+  shelby: shelby,
+  seat: seat,
+  scion: scion,
+  schuppan: schuppan,
+  saturn: saturn,
+  saleen: saleen,
+  saab: saab,
+  ruf: ruf,
+  rover: rover,
+  rossion: rossion,
+  rollsroyce: rollsroyce,
+  robby_gordon: robby_gordon,
+  rj: rj,
+  rimac: rimac,
+  reliant: reliant,
+  ram: ram,
+  raesr: raesr,
+};
+
+export default manufacturer2;
