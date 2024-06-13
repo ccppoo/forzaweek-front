2ba1b34dd556f01c245a8cb5e1b80d8e1c36ddb2
ccppoo <shmishmi79@gmail.com>
2024-06-13 23:34:08 +0900

    i18n -> en, ko

--- src/FormData/i18n.ts
+++ src/FormData/i18n.ts
@@ -1,14 +1,14 @@
 import { z } from 'zod';
 
-export const i18nLangCode = z.object({
-  code: z.string(),
-  country: z.union([z.string(), z.literal('')]),
-});
+// export const i18nLangCode = z.object({
+//   code: z.string(),
+//   country: z.union([z.string(), z.literal('')]),
+// });
 
 export const i18nTextFieldSchema = z.object({
-  lang: i18nLangCode,
+  lang: z.string(),
   value: z.string(),
 });
 
-export type I18nLangCode = z.infer<typeof i18nLangCode>;
+// export type I18nLangCode = z.infer<typeof i18nLangCode>;
 export type I18nTextFieldSchema = z.infer<typeof i18nTextFieldSchema>;
