c13de576b6c644437f027a5a46bd0be62e851464
ccppoo <shmishmi79@gmail.com>
2024-06-14 17:19:14 +0900

    form update - nation, manufacturer

--- src/FormData/manufacturer.ts
+++ src/FormData/manufacturer.ts
@@ -4,18 +4,22 @@ import { supportLangs } from '@/config/i18n';
 
 import { i18nTextFieldSchema } from './i18n';
 
-export const nationEditSchema = z.object({
+export const manufacturerEditSchema = z.object({
   id: z.optional(z.string()),
   imageURL: z.optional(z.string()),
+  origin: z.optional(z.string()),
+  founded: z.optional(z.number()),
   i18n: z.array(i18nTextFieldSchema),
   name_en: z.optional(z.string()),
 });
 
-export type NationEditSchema = z.infer<typeof nationEditSchema>;
+export type ManufacturerEditSchema = z.infer<typeof manufacturerEditSchema>;
 
-export const nationEditSchemaDefault: NationEditSchema = {
+export const manufacturerEditSchemaDefault: ManufacturerEditSchema = {
   imageURL: undefined,
   name_en: undefined,
+  origin: undefined,
+  founded: undefined,
   i18n: [...supportLangs].map((langDefault) => {
     return { lang: langDefault, value: '' };
   }),
