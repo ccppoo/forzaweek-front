3880ea372a28b403dfc5ef18f9af9c719a98a998
ccppoo <shmishmi79@gmail.com>
2024-06-20 22:56:33 +0900

    image upload api

--- src/api/data/image.ts
+++ src/api/data/image.ts
@@ -32,3 +32,33 @@ export async function UploadTempImage({
 
   return image;
 }
+
+// URL 반환하는 이미지
+export async function UploadImage({
+  folder,
+  fileBlobURL,
+}: {
+  folder: string;
+  fileBlobURL: string;
+}): Promise<string> {
+  const formData = new FormData();
+
+  const response = await fetch(fileBlobURL);
+  const imgFile = await response.blob();
+  formData.append('file', imgFile);
+
+  const path_ = `image/${folder}`;
+
+  const url = `${API_HOST}/${path_}`;
+
+  const resp = await axios.post(url, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+
+  const { image } = resp.data;
+  // console.log(`resp.data.image : ${image}`);
+
+  return image;
+}
