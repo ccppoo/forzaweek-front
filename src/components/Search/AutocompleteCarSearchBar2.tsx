6ce1f505f6ec52fc047c11abf46eca13ad69d2f7
ccppoo <shmishmi79@gmail.com>
2024-06-25 23:08:37 +0900

    차 하나 선택 후 추가 기능

---
+++ src/components/Search/AutocompleteCarSearchBar2.tsx
@@ -0,0 +1,104 @@
+import { useState } from 'react';
+
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
+import { useQuery } from '@tanstack/react-query';
+import { useLiveQuery } from 'dexie-react-hooks';
+
+import { FlexBox } from '@/components/styled';
+import { db, getCarInfo, getCarInfoByID } from '@/db';
+import type { Car, CarImage, FH5_STAT } from '@/db/schema';
+import useCarAndTagFilter from '@/store/carAndTagFilter';
+import useCarSearchFilters, { CarSearchOption } from '@/store/carSearchFilters';
+
+async function getCarData(): Promise<Car[]> {
+  return await db.car.toArray();
+}
+
+interface AutocompleteCarSearchBar2Intf {
+  searchScope: string;
+  onSelect: (car: Car) => void;
+}
+
+export default function AutocompleteCarSearchBar2(props: AutocompleteCarSearchBar2Intf) {
+  // 직접 검색해서 찾을 수 있는 검색 바
+  // TODO: Selection -> DB에서 자동차 ID로 저장 + 하나 선택했으면 Search Filter 업데이트하기
+
+  const { searchScope, onSelect } = props;
+
+  const {
+    actions: {
+      car: { setCar },
+    },
+  } = useCarAndTagFilter(searchScope);
+  const options = useLiveQuery(getCarData) || [];
+  const [carValue, setCarValue] = useState<Car | undefined>(undefined);
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const submitToCarTagFilter = async (carID: number) => {
+    const car = await getCarInfoByID(carID);
+    setCar(car);
+  };
+
+  return (
+    <FlexBox sx={{ width: '100%', paddingY: 1, paddingX: 3, justifyContent: 'center' }}>
+      <FlexBox sx={{ justifyContent: 'center', alignItems: 'center', paddingX: 1 }}>
+        <SearchOutlinedIcon />
+      </FlexBox>
+      <Autocomplete
+        id="tags-outlined"
+        size="small"
+        options={options}
+        filterOptions={(options, state) => {
+          const displayOptions = options.filter((option) => {
+            const search = state.inputValue.toLowerCase().trim();
+            return [
+              option.name.toLowerCase().trim().includes(search),
+              option.manufacture.toLowerCase().trim().includes(search),
+              option.country.toLowerCase().trim().includes(search),
+              option.model.toLowerCase().trim().includes(search),
+            ].some((x) => x);
+          });
+          return displayOptions;
+        }}
+        groupBy={(option: Car) => option.manufacture}
+        getOptionLabel={(option: Car) => option.name}
+        defaultValue={null}
+        autoSelect
+        value={carValue}
+        onChange={(event, newValue: Car | string | null) => {
+          if (newValue === null) {
+            // 지우는 경우 무시
+            setInputValue('');
+            return;
+          }
+          if (typeof newValue === 'string') {
+            // 메뉴에 있는거만 선택 강제
+            setInputValue('');
+            return;
+          }
+          // 기존에 있는 옵션 선택할 경우
+          onSelect(newValue);
+          setInputValue('');
+          setCarValue(undefined);
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue, reason) => {
+          console.log(`4`);
+          setInputValue(newInputValue);
+          console.log(`reason : ${reason}`);
+          if (reason == 'reset') {
+            setInputValue('');
+            setCarValue(undefined);
+          }
+        }}
+        filterSelectedOptions
+        renderInput={(params) => <TextField {...params} placeholder={'Search car'} sx={{}} />}
+        sx={{ width: '100%' }}
+      />
+    </FlexBox>
+  );
+}
---
+++ src/components/Search/CarSearchAndSelectDialog.tsx
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import { useForm, useFormContext } from 'react-hook-form';
+
+import { Button, Dialog, DialogActions, Typography } from '@mui/material';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import AutocompleteCarSearchBar2 from '@/components/Search/AutocompleteCarSearchBar2';
+import { FlexBox, FullSizeCenteredFlexBox } from '@/components/styled';
+import { Image } from '@/components/styled';
+import type { Car } from '@/db/schema';
+
+interface CarSearchAndSelectDialogIntf {
+  selectScope: string;
+  opened: boolean;
+  onClose: () => void;
+}
+
+export default function CarSearchAndSelectDialog(props: CarSearchAndSelectDialogIntf) {
+  const { selectScope, opened, onClose: closeDialog } = props;
+
+  const {
+    control,
+    formState: { errors },
+    setValue,
+  } = useFormContext();
+  const dialogWidth = 'md';
+
+  type CloseReason = 'backdropClick' | 'escapeKeyDown';
+  const handleClose = (event: object, reason: CloseReason) => {
+    if (reason && reason === 'backdropClick') return;
+    closeDialog();
+  };
+  const [carSelected, setCarSelected] = useState<Car | undefined>();
+
+  const onSelectCar = (car: Car) => {
+    setCarSelected(car);
+  };
+
+  const CarName = carSelected?.name || 'Car Not Selected';
+
+  const submitCarSelected = () => {
+    setValue('car', carSelected?.name);
+    closeDialog();
+  };
+
+  return (
+    <Dialog open={opened} maxWidth={dialogWidth} fullWidth onClose={handleClose}>
+      <DialogTitle>Select Car</DialogTitle>
+      <FlexBox sx={{ flexDirection: 'column', minHeight: 160, rowGap: 1 }}>
+        <FlexBox sx={{ width: '100%', height: '100%' }}>
+          <AutocompleteCarSearchBar2 searchScope={selectScope} onSelect={onSelectCar} />
+        </FlexBox>
+        {/* 선택한 차 -> 사진 + 간단 정보 보여주고 ->  Select 하면 Form에 저장하기 */}
+        <FlexBox
+          sx={{ height: 100, width: '100%', paddingX: 4, alignItems: 'center', columnGap: 2 }}
+        >
+          {/* 사진 올 자리 */}
+          <FlexBox sx={{ height: '100%', width: 200, border: '1px solid black' }}>
+            <Image sx={{ backgroundColor: '#DDDDDD' }} />
+          </FlexBox>
+          <FlexBox
+            sx={{
+              flexDirection: 'column',
+              height: '100%',
+              width: '100%',
+            }}
+          >
+            <FlexBox sx={{ height: '100%' }}>
+              <Typography>{CarName}</Typography>
+            </FlexBox>
+            {/* 제조사  */}
+            {/* {carSelected && JSON.stringify(carSelected)} */}
+          </FlexBox>
+        </FlexBox>
+      </FlexBox>
+      <DialogActions>
+        <Button variant="outlined" color="error" onClick={closeDialog}>
+          Close
+        </Button>
+        <Button variant="contained" onClick={submitCarSelected}>
+          Select Car
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
