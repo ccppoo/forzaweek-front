2276d734658346abf8a0931ea7a8702027af6c2b
ccppoo <shmishmi79@gmail.com>
2024-06-27 22:14:08 +0900

    update utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -13,4 +13,22 @@ function sortPIClass(piClass1: PIClass | string, piClass2: PIClass | string) {
   return pi[piClass1 as PIClass] - pi[piClass2 as PIClass];
 }
 
-export { sortPIClass };
+// const range = function* (to = 0, from = 0, step = 1) {
+//   let i = 0,
+//     length = Math.floor((to - from) / step) + 1;
+//   while (i < length) yield from + i++ * step;
+// };
+
+const range = function* (start: number, stop: number | null, step = 1) {
+  if (stop == null) {
+    // one param defined
+    stop = start;
+    start = 0;
+  }
+
+  for (let i = start; step > 0 ? i < stop : i > stop; i += step) {
+    yield i;
+  }
+};
+
+export { sortPIClass, range };
