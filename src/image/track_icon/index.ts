85b973c16502a26b85f4fb7061841e44625d5758
ccppoo <shmishmi79@gmail.com>
2024-07-21 22:35:45 +0900

    Create index.ts

---
+++ src/image/track_icon/index.ts
@@ -0,0 +1,33 @@
+import colossus from './colossus.png';
+import crosscountry_sprint from './crosscountry_sprint.png';
+import crosscountry_track from './crosscountry_track.png';
+import drag_racing from './drag_racing.png';
+import gauntlet from './gauntlet.png';
+import goliath from './goliath.png';
+import juggernaut from './juggernaut.png';
+import marathon from './marathon.png';
+import offroad_sprint from './offroad_sprint.png';
+import offroad_track from './offroad_track.png';
+import road_sprint from './road_sprint.png';
+import road_track from './road_track.png';
+import street_racing from './street_racing.png';
+import titan from './titan.png';
+
+export const trackIcons = {
+  crosscountry_sprint,
+  crosscountry_track,
+  drag_racing,
+  offroad_sprint,
+  offroad_track,
+  road_sprint,
+  road_track,
+  street_racing,
+  event: {
+    colossus,
+    gauntlet,
+    goliath,
+    juggernaut,
+    marathon,
+    titan,
+  },
+};
