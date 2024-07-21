505c7512b6abe75d4dfde2c19aaeec7efd2965ad
ccppoo <shmishmi79@gmail.com>
2024-07-21 22:36:11 +0900

    Create index.ts

---
+++ src/types/fh5/index.ts
@@ -0,0 +1,18 @@
+export type World = 'Mexico' | 'Hot Wheels' | 'Sierra Nueva';
+export const Worlds: World[] = ['Mexico', 'Hot Wheels', 'Sierra Nueva'];
+
+export type TrackFormat = 'sprint' | 'trail' | 'course' | 'circuit' | 'scramble';
+export const TrackFormats: TrackFormat[] = ['sprint', 'trail', 'course', 'circuit', 'scramble'];
+
+export type TrackFormatTopology = 'linear' | 'circular';
+export const TrackFormatTopologies: TrackFormatTopology[] = ['linear', 'circular'];
+
+export type TrackCategory = 'cross country' | 'rally' | 'off-road' | 'road' | 'street' | 'drag';
+export const TrackCategories: TrackCategory[] = [
+  'cross country',
+  'rally',
+  'off-road',
+  'road',
+  'street',
+  'drag',
+];
