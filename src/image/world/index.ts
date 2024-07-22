9aed301fbf01e802e79c679565e4f252d84e34b0
ccppoo <shmishmi79@gmail.com>
2024-07-22 22:23:25 +0900

    add FH5 world image

---
+++ src/image/world/index.ts
@@ -0,0 +1,11 @@
+import hot_wheels from './fh5_hot_wheels.png';
+import mexico from './fh5_mexico.png';
+import rally from './fh5_rally.png';
+
+export const world_logo = {
+  fh5: {
+    mexico,
+    hot_wheels,
+    rally,
+  },
+};
