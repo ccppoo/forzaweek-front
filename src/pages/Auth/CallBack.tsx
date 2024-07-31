916e89fc59132cf9ee191cb341ab39a75819f130
ccppoo <shmishmi79@gmail.com>
2024-07-31 23:47:30 +0900

    login, oauth callback page

--- src/pages/Auth/CallBack.tsx
+++ src/pages/Auth/CallBack.tsx
@@ -1,37 +1,54 @@
 import { useEffect } from 'react';
-import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 import { Box, Button, ButtonBase, Paper, Typography } from '@mui/material';
 import Container from '@mui/material/Container';
 
 import { useQuery } from '@tanstack/react-query';
 
-import { Callback as CallbackOauth } from '@/api/auth/callback';
+import * as xbox from '@/api/auth/xbox';
 import { FlexBox, FullSizeCenteredFlexBox, Image } from '@/components/styled';
-import xboxIcon from '@/image/xbox.png';
+import useAuthState from '@/store/auth';
 
-const sections = ['NEWS', 'Week Festa', 'Tunings'];
-export const openInNewTab = (url: string): void => {
-  const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
-  if (newWindow) newWindow.opener = null;
+const closeThisTab = (): void => {
+  window.close();
 };
 
 export default function CallBack() {
-  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const oauth_code = searchParams.get('code'); //
-  // console.log(`oauth_code : ${oauth_code}`);
+
+  const [_, state, { setAuthTokens }] = useAuthState();
 
   const { data, isLoading, isSuccess, isError } = useQuery({
     queryKey: ['oauth callback', oauth_code!],
-    queryFn: CallbackOauth,
+    queryFn: xbox.Callback,
     retry: false,
     enabled: !!oauth_code,
+    staleTime: Infinity,
   });
 
   console.log(`data : ${JSON.stringify(data)}`);
 
+  if (isLoading) {
+    return (
+      <FullSizeCenteredFlexBox sx={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Paper sx={{ width: 400, height: 600 }}>
+          <FlexBox sx={{ width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+            <Typography>Loading...</Typography>
+          </FlexBox>
+        </Paper>
+      </FullSizeCenteredFlexBox>
+    );
+  }
+
   // 할 것 - 백엔드에서 Token 또는 인증 성공시 탭 닫기 (원래 이전에 있던 창으로 돌아가기)
+  if (isSuccess && data) {
+    console.log(`isSuccess`);
+    setAuthTokens(data);
+    // closeThisTab();
+  }
+
   return (
     <Container sx={{ height: '100%' }}>
       <FullSizeCenteredFlexBox sx={{ alignItems: 'center', justifyContent: 'center' }}>
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,41 +1,32 @@
 import { useEffect } from 'react';
-import MicrosoftLogin from 'react-microsoft-login';
-import { useNavigate } from 'react-router-dom';
 
 import { Box, Button, ButtonBase, Paper, Typography } from '@mui/material';
 import Container from '@mui/material/Container';
 
-import { Login as LoginOauth } from '@/api/auth/login';
+import * as xbox from '@/api/auth/xbox';
 import { FlexBox, FullSizeCenteredFlexBox, Image } from '@/components/styled';
 import xboxIcon from '@/image/xbox.png';
+import useAuthState from '@/store/auth';
 
-export const openInNewTab = (url: string): void => {
+const openInNewTab = (url: string): void => {
   const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
   if (newWindow) newWindow.opener = null;
 };
 
 export default function Login() {
-  const navigate = useNavigate();
-
   const iconSize = 50;
   const borderRadius = 2;
+  const [] = useAuthState(); // redirect 받고 로그인 된 것 확인하면 원래 있던 path로 되돌려 보내기
 
   const onClickLogin = async () => {
-    const { redirectTo } = await LoginOauth();
+    const { redirectTo } = await xbox.Login();
     console.log(`redirectTo : ${redirectTo}`);
     openInNewTab(redirectTo);
   };
 
-  // const SCOPES = ['Xboxlive.signin', 'Xboxlive.offline_access'];
-  const SCOPES = ['offline_access'];
-  const REDIRECT_URI = 'https://localhost:5173/auth/callback';
-  // const REDIRECT_URI = 'https://localhost:8080/auth/callback';
   return (
     <Container sx={{ height: '100%' }}>
       <FullSizeCenteredFlexBox sx={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Button variant="contained" onClick={onClickLogin}>
-          xbox oauth go
-        </Button>
         <Paper sx={{ width: 400, height: 600 }}>
           <FlexBox sx={{ width: '100%', alignItems: 'center', justifyContent: 'center' }}>
             <Typography>로그인</Typography>
