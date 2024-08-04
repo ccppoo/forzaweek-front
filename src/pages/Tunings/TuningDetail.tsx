18212a80ec31d9c8e8ec443fb91b901338b2d21c
ccppoo <shmishmi79@gmail.com>
2024-08-04 23:18:50 +0900

    comments copmonent update

--- src/pages/Tunings/TuningDetail.tsx
+++ src/pages/Tunings/TuningDetail.tsx
@@ -16,7 +16,7 @@ import { useQuery } from '@tanstack/react-query';
 import * as image from '@/image';
 import { GetTuning, SearchTunings } from '@/api/data/tuning';
 import { BriefCarInfo } from '@/components/Car';
-import Comments from '@/components/Comment';
+import { VotableComments } from '@/components/Comment';
 import SharingCreationCreator from '@/components/Post/Creator';
 import ShareCode from '@/components/Post/ShareCode';
 import Tags from '@/components/Post/Tags';
@@ -63,7 +63,7 @@ export default function TuningDetail() {
             <RadarChartAndPerformance tuning={data} />
             <Divider flexItem variant="fullWidth" />
             {/* 댓글 */}
-            <Comments />
+            <VotableComments subject_to={tuningID!} />
             {/* 관련 영상 */}
             <RelatedVideos />
             {/* 관련 다른 튜닝들 */}
