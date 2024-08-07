aa19f2654202b1a6e43b46b3f2dd8fe4adc18526
ccppoo <shmishmi79@gmail.com>
2024-08-07 22:26:59 +0900

    editor image api and config

--- src/api/image.ts
+++ src/api/image.ts
@@ -3,24 +3,60 @@ import type { AxiosResponse } from 'axios';
 
 import { API_HOST, AuthHeaders } from '@/api/index';
 import type { API_NAME } from '@/api/types';
+import type {
+  BoardImageUploadType,
+  RemoveBoardImageType,
+  UploadByFileType,
+} from '@/components/Editor/types';
 
-async function uploadByFile(file: File) {
-  const formData = new FormData();
-  formData.append('file', file);
+const getBoardCreateImageUploader = (token: string): UploadByFileType => {
+  const authHeaders = AuthHeaders(token);
 
-  return {
-    success: 1,
-    file: {
-      // url: await Utils.returnBase64FromFile(file),
-    },
-  };
-}
+  async function uploadByFile(file: File): Promise<BoardImageUploadType> {
+    const folder = `board/upload`;
 
-type BoardImageUploadType = {
-  success: number;
-  file: {
-    url: string;
-  };
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const path_ = `image/${folder}`;
+    const url = `${API_HOST}/${path_}`;
+
+    const resp = await axios.post(url, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        ...authHeaders,
+      },
+    });
+
+    return resp.data;
+  }
+
+  return uploadByFile;
+};
+
+const getBoardCreateImageRemover = (token: string): RemoveBoardImageType => {
+  const authHeaders = AuthHeaders(token);
+
+  function RemoveBoardImage(image_url: string): void {
+    const folder = `board/delete`;
+    const path_ = `image/${folder}`;
+    const url = `${API_HOST}/${path_}`;
+
+    const data = {
+      url: image_url,
+    };
+    const resp = axios
+      .post(url, data, {
+        headers: {
+          ...authHeaders,
+        },
+      })
+      .then()
+      .catch();
+    // return;
+  }
+
+  return RemoveBoardImage;
 };
 
 export async function UploadBoardImage(file: File): Promise<BoardImageUploadType> {
@@ -30,12 +66,13 @@ export async function UploadBoardImage(file: File): Promise<BoardImageUploadType
   formData.append('file', file);
 
   const path_ = `image/${folder}`;
-
+  // const authHeaders = AuthHeaders(token);
   const url = `${API_HOST}/${path_}`;
 
   const resp = await axios.post(url, formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
+      // ...authHeaders,
     },
   });
 
@@ -50,3 +87,10 @@ export function RemoveBoardImage(removedImgUrl: string): void {
   const resp = axios.delete(url, {}).then();
   // return;
 }
+
+export default {
+  getBoardCreate: {
+    imageUploader: getBoardCreateImageUploader,
+    imageRemover: getBoardCreateImageRemover,
+  },
+};
--- src/components/Editor/config/index.ts
+++ src/components/Editor/config/index.ts
@@ -1,30 +1,44 @@
 import type { EditorConfig, ToolboxConfig } from '@editorjs/editorjs';
 import type { OutputData } from '@editorjs/editorjs';
 
+import type {
+  BoardImageUploadType,
+  RemoveBoardImageType,
+  UploadByFileType,
+} from '@/components/Editor/types';
+
 import type { onChangeEditorJS } from '../types';
-import { embedTools, imageTools } from './mediaTools';
+import { embedTools, getImageTools } from './mediaTools';
 import basicEditorTools from './tools';
 
 interface EditorConfigIntf {
   holder: string;
   data: OutputData;
   defaultBlock: string;
-  imageUpload?: boolean;
   embedAllow?: boolean;
+  uploadByFile?: UploadByFileType;
+  removeImageFromBlock?: RemoveBoardImageType;
   onChange: onChangeEditorJS;
   onReady: () => void;
 }
 
 const getEditorConfig: (props: EditorConfigIntf) => EditorConfig = (props: EditorConfigIntf) => {
-  const { imageUpload, embedAllow, ...rest } = props;
+  // 이미지 업로드 가능한 게시물의 경우 이 2개가 모두 있어야 함 uploadByFile, removeImageFromBlock
+  const { uploadByFile, removeImageFromBlock, embedAllow, ...rest } = props;
 
-  // console.log(`imageUpload : ${imageUpload}`);
-  // console.log(`embedAllow : ${embedAllow}`);
+  const imageSupported = uploadByFile && removeImageFromBlock;
+
+  const tempImageRemover = (image_url: string) => {
+    console.log(`image_url : ${image_url}`);
+  };
+  const _imageTool = !!imageSupported
+    ? getImageTools(uploadByFile, removeImageFromBlock)
+    : undefined;
   return {
     ...rest,
     tools: {
       ...basicEditorTools,
-      image: !!imageUpload ? imageTools : undefined,
+      image: _imageTool,
     },
   } as unknown as EditorConfig;
 };
